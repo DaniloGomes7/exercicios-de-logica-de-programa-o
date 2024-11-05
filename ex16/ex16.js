cb3b1a3c663f40833a6b60d62163637d0478be5c
DaniloGomes7 <danilo.gomes48@etec.sp.gov.br>
2024-11-05 19:56:04 -0300

    exercicios 1 ao 16 (13 incompleta )

--- ex16/ex16.js
+++ ex16/ex16.js
@@ -1,8 +1,10 @@
-var MetrosPintados = parseInt(prompt("Quantos metros quadrados serão pintados"))
-var CoberturaTinta = MetrosPintados/54
-var LataTinta = CoberturaTinta*80
+var MetrosPintados = parseInt(prompt("Quantos metros quadrados serão pintados? "));
+var lataqntd = MetrosPintados/54 ;
+var latapreco = lataqntd * 80 ;
 
-if (MetrosPintados =>54) {
-    console.log("A quantidade de latas de tinta a ser comprada é de ",MetrosPintados,", Custando ",LataTinta)
-    alert("A quantidade de latas de tinta a ser comprada é de "+MetrosPintados+", Custando "+LataTinta)
-} 
+
+if (MetrosPintados=>54){
+    console.log ("A quantidade de latas que voce usara é ",lataqntd,",o preco a ser pago é ",latapreco);
+    alert ("o preco a ser pago é "+latapreco)
+    alert ("A quantidade de latas que voce usara é "+lataqntd)
+}
